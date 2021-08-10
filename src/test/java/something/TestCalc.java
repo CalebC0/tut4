69c5194e82ae0eb3240681d792d5fa78daadc6e3
Caleb <calebpersonal0@gmail.com>
2021-08-10 23:52:18 +1200

    Added a bad test that fails

--- src/test/java/something/TestCalc.java
+++ src/test/java/something/TestCalc.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCalc {
+	private Calc tester = new Calc();
 	@Test
 	public void testAdd() {
-		Calc tester = new Calc();
 		assertEquals(tester.add(2, 2), 4);
+	}
+	@Test
+	public void testSubtract() {
+		assertEquals(tester.subtract(3, 1), 2);
 	}
 }
